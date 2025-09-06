2e7aa30e3669a2c8ef3e202bc421f6ad7674a968
e-asimate <emmanuelasimate125@gmail.com>
2025-09-06 03:29:26 +0000

    Update streamlit_app.py

--- physics-viz-ml/app/streamlit_app.py
+++ physics-viz-ml/app/streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import joblib
+try:
+    import joblib
+except ModuleNotFoundError:
+    joblib = None
 import os, sys
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT not in sys.path:
